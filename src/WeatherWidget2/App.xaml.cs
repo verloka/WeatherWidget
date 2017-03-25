8562533522dc51b4c92d34a4e3df42681144e59e
ogycode <verlokav@gmail.com>
2017-03-25 21:41:09 +0200

    Added options view and info block view

--- src/WeatherWidget2/App.xaml.cs
+++ src/WeatherWidget2/App.xaml.cs
@@ -10,6 +10,26 @@ namespace WeatherWidget2
 {
     public partial class App : Application
     {
-        public static Model.Localization Lang = new Model.Localization();
+        public static Model.Localization Lang;
+        public static Verloka.HelperLib.Settings.RegSettings Settings;
+
+        void AppStartup(object sender, StartupEventArgs e)
+        {
+            bool silent = false;
+            for (int i = 0; i != e.Args.Length; ++i)
+                if (e.Args[i] == "-silent")
+                    silent = true;
+
+
+            //INIT block
+            Lang = new Model.Localization();
+            Settings = new Verloka.HelperLib.Settings.RegSettings("Weather widget 2");
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.WindowState = silent ? WindowState.Minimized : WindowState.Normal;
+            mainWindow.Show();
+            if (silent)
+                mainWindow.Hide();
+        }
     }
 }
--- src/WeatherWidget2/Model/Localization.cs
+++ src/WeatherWidget2/Model/Localization.cs
@@ -5,6 +5,9 @@ namespace WeatherWidget2.Model
     [DataContract(Name = "Language File")]
     public class Localization
     {
+        [DataMember(Name = "LanguageName")]
+        public string LanguageName { get; set; } = "!default";
+
         [DataMember(Name = "GeneralTabHome")]
         public string GeneralTabHome { get; set; } = "!home";
         [DataMember(Name = "GeneralTabWidgets")]
@@ -16,5 +19,19 @@ namespace WeatherWidget2.Model
 
         [DataMember(Name = "TabHomeWhatNew")]
         public string TabHomeWhatNew { get; set; } = "!what new?";
+
+        [DataMember(Name = "TabOptionsAutorun")]
+        public string TabOptionsAutorun { get; set; } = "!startup with windows";
+        [DataMember(Name = "TabOptionsMsgInet")]
+        public string TabOptionsMsgInet { get; set; } = "!show message when no internet";
+        [DataMember(Name = "TabOptionsCheckUpdate")]
+        public string TabOptionsCheckUpdate { get; set; } = "!check update with startup";
+        [DataMember(Name = "TabOptionsLanguage")]
+        public string TabOptionsLanguage { get; set; } = "!lang";
+        [DataMember(Name = "TabOptionsTheme")]
+        public string TabOptionsTheme { get; set; } = "!theme";
+
+        [DataMember(Name = "TabInfoDeveloped")]
+        public string TabInfoDeveloped { get; set; } = "!developed in 2017 by Verloka Vadim";
     }
 }
